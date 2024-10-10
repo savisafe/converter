62f99ecd5043580b1b442c5c04134a0e578e6669
savisafe <savisafe@vk.com>
2024-10-10 20:57:21 +0500

    fix: exel to json

--- scripts/exelToJson.js
+++ scripts/exelToJson.js
@@ -1,4 +1,21 @@
+import * as XLSX from 'xlsx';
 
-var parser = require('simple-excel-to-json')
-var doc = parser.parseXls2Json('./exel/exel-tojson.xlsx');
-console.log(doc);
+document.getElementById('input').addEventListener('change', handleFile, false);
+
+function handleFile(event) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function(e) {
+        const data = new Uint8Array(e.target.result);
+        const workbook = XLSX.read(data, { type: 'array' });
+
+        const sheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[sheetName];
+        const json = XLSX.utils.sheet_to_json(worksheet);
+
+        console.log(json);
+    };
+
+    reader.readAsArrayBuffer(file);
+}
